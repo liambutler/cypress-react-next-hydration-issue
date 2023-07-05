cbebe97af2a657378f20797289a9313fe23f29c9
Liam Butler <liam.butler@lego.com>
2023-07-05 14:10:51 +0200

    Basic repro

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -10,13 +10,7 @@ describe('Navigation', () => {
     cy.visit('http://localhost:3000/')
 
     // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click()
-
-    // The new url should include "/about"
-    cy.url().should('include', '/about')
-
-    // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
+    cy.wait(5000)
   })
 })
 
